621e3ada110ec15350477cd4098c4321e81d1823
matthew vinck <matthew.vinck@outlook.com>
2018-02-25 22:06:43 -0600

    test emal VER2
    Test emai8l succesfully wernt through after allowing google to allow less secure apps to send emails

--- Test.Email.py
+++ Test.Email.py
@@ -1,11 +1,13 @@
 # Import smtplib for the actual sending function
-import smtplib
+import smtplib, os
 
 # Import the email modules we'll need
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login("matthewvinck@gmail.com", "mattywow0123")
-  
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 f = open(dir_path + '\output.txt', 'r+')
 buffer = f.read()
 f.close
